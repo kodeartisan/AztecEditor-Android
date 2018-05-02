24cb1e49e74ae9253e4a37dc3a9bc5ca3e64673f
Stefanos Togkoulidis <stefanostogoulidis@gmail.com>
2018-05-02 04:44:12 +0300

    Don't try to clear the focus

--- aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
+++ aztec/src/main/kotlin/org/wordpress/aztec/AztecText.kt
@@ -1242,7 +1242,6 @@ open class AztecText : AppCompatEditText, TextWatcher, UnknownHtmlSpan.OnUnknown
             android.R.id.pasteAsPlainText -> paste(text, min, max)
             android.R.id.copy -> {
                 copy(text, min, max)
-                clearFocus() // hide text action menu
             }
             android.R.id.cut -> {
                 copy(text, min, max)
